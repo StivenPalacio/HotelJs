c06d634663cf4913a44e7f70a91fd3663e85dac3
Stiven Palacio <stivenchoo11@gmail.com>
2022-06-22 06:34:40 -0500

    V4 Ok para subir a la nube

---
+++ js/hotel.js
@@ -0,0 +1,14 @@
+function Img1(){
+    const Img1 = document.querySelector('#habitacion')
+    Img1.src = './img/habitacion2.jpg'
+}
+function Img2(){
+    const Img2 = document.querySelector('#habitacion')
+    Img2.src = './img/habitacion1.jpg'
+}
+
+function cotizar() {
+    const nroPer = document.querySelector('#nroPer')
+    const nroDias = document.querySelector('#nroDias')
+    const vlrPer = document.querySelector('#vlrPer')
+}
